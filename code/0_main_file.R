f745cbfd78e7c91f189ebea4356067dbebbde5a9
AnneMeyerEpi <146199578+AnneMeyerEpi@users.noreply.github.com>
2025-04-26 09:43:09 +0200

    Adjust main file to create sub-directories

--- code/0_main_file.R
+++ code/0_main_file.R
@@ -21,11 +21,8 @@ library(officer)
 rm(list=ls())
 set.seed(123)
 setwd(choose.dir()) # choose "feedlot_model" directory location
-
-# choose a local directory to save outputs, plots, etc. (not sync'd with Github)
-# make sure it has two sub-directories in it: outputs and figures
-local_folder <- choose.dir()
-local_folder <- paste0(gsub("\\\\", "/", local_folder),"/")
+dir.create("outputs")
+dir.create("figures")
 
 # Load model code
 source("code/1_ahle_model_growout_v6.R")
@@ -33,7 +30,6 @@ source("code/1_ahle_model_growout_v6.R")
 ## Run model for each scenario
 scenario_file <- "data/example_params_feedlot.xlsx"
 source("code/2_run_model.R")
-rm(list=ls()[! ls() %in% c("local_folder")])
 
 ## Process outputs and create tables and plots
 source("code/3_process_ahle_data.R")
